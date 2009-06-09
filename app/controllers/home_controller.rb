872e6ea212a9654f1212408537eb95eaa9961ea5
christian <christian@hager.as>
2009-06-09 11:17:52 +0200

    regular commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,19 @@
 class HomeController < ApplicationController
   def index
+    @mykurls = []
+    
+    
+          cookies[:mykurls].each do |cookie|
+      
+                @tmp_url = Mykurl.find_by_token(cookie[:value]);
+                
+                unless @tmp_url.nil?
+                  @mykurls << @tmp_url
+                end
+    
+           end
+    
+    
     @mykurl = Mykurl.new
   end
   
--- app/controllers/mykurls_controller.rb
+++ app/controllers/mykurls_controller.rb
@@ -52,19 +52,25 @@ class MykurlsController < ApplicationController
     @mykurl = Mykurl.find(params[:id])
   end
 
-  # POST /mykurls
+  # POST /mykurls.
   # POST /mykurls.xml
   def create
     @mykurl = Mykurl.new(params[:mykurl])
     @mykurl.token = Mykurl.generate_token
     respond_to do |format|
       if @mykurl.save
+        #create a coockie if user not logged in, save for 2 weeks
+        unless current_user
+        cookies[:mykurls] = { :value => @mykurl.token, :expires => Time.now + ((3600*24)*7*2)}
+        end
+        
         flash[:notice] = 'Url was successfully shortened.'
         format.html { redirect_to(@mykurl) }
         format.js { return :text => "Url shortened"  }
         format.xml  { render :xml => @mykurl, :status => :created, :location => @mykurl }
       else
         flash[:notice] = 'Mykurl was NOT successfully created.'
+        
         format.html { render :action => "new" }
         format.js { return :text => "Url was not shortened"  }
         format.xml  { render :xml => @mykurl.errors, :status => :unprocessable_entity }
